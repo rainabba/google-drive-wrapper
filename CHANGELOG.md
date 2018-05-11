2fc3dec1a1c76fc8a39af00ecb4527ba43c24160
Michael-Rainabba Richardson <rainabba@gmail.com>
2018-05-11 13:30:41 -0700

    Note: 	BREAKING CHANGES - this would be a major version increment IF it was 	released. If you were using a prior version, you will want to see test/index.js 	and make note of where promises are now used and how to call the 	constructor.
    Fix:
    	- Add more error checking and handling in constructor
    	- Address memory leak with fileComponents object
    
    Features
    	- Fully reworked tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,18 @@ Features
 	- Promisify all public methods in the library
 	- Update googleapis to v29.0.0
 	- Added tests to confirm some of the functionality - Lots more tests needed
+
+### 0.8.0 ( )
+
+Note:
+	BREAKING CHANGES - this would be a major version increment IF it was
+	released. If you were using a prior version, you will want to see test/index.js
+	and make note of where promises are now used and how to call the
+	constructor.
+
+Fix:
+	- Add more error checking and handling in constructor
+	- Address memory leak with fileComponents object
+
+Features
+	- Fully reworked tests
--- test/index.js
+++ test/index.js
@@ -15,11 +15,22 @@ let Code = require('code'),
     expect = Code.expect,
     server = null,
     googleAuthCredentials = path.normalize(path.join(__dirname, '../private/client_secret.json')),
-    auth = null;
+    auth = null,
+    driveWrapper = null;
 
-require('oauth-token-generator-google')(googleAuthCredentials).then(_auth => { auth = _auth; }).catch(err => { console.dir(err) });
 
 lab.experiment("Google API ", {}, () => {
+
+    lab.before( () => {
+        return new Promise( (resolve,reject) => {
+            require('oauth-token-generator-google')(googleAuthCredentials).then( _auth => {
+                let wrapper = require('../lib/gdriveWrapper.js');
+                driveWrapper = new wrapper( _auth, google);
+                auth = _auth;
+                resolve();
+            }).catch(err => { console.dir(err) });
+        })
+    });
 
     lab.test("confirm credentials using Drive filelist", { timeout: 5000 }, () => {
 
@@ -29,12 +40,14 @@ lab.experiment("Google API ", {}, () => {
                 auth: auth,
                 pageSize: 10,
                 fields: 'nextPageToken, files(id, name)'
-            }, (err, res) => {
+            }, ( err, res ) => {
                 if (err) {
                     console.error('The API returned an error.');
                     console.dir(err);
                     reject(err);
                 }
+                expect(res).to.be.an.object();
+                expect(res.data).to.be.an.object();
                 expect(res.data.files).to.be.an.array();
                 expect(res.data.files.length).to.be.greaterThan(0);
                 resolve();
@@ -46,11 +59,10 @@ lab.experiment("Google API ", {}, () => {
     lab.test(" uploading /uploadTest.txt", { timeout: 10000 }, () => {
 
         return new Promise((resolve, reject) => {
-            let gdriveWrapper = require('../lib/gdriveWrapper.js'),
-                wrapper = new gdriveWrapper(auth, google),
-                uploadFile = path.normalize(path.join(__dirname, 'uploadTest.txt'));
+            let uploadFile = path.normalize(path.join(__dirname, 'uploadTest.txt'));
             expect(fs.existsSync(uploadFile)).to.be.true();
-            wrapper.uploadFile('uploadTest.txt', uploadFile, { resource: { description: 'Google API uploadTest.txt test' }, properties: { testProp: "testValue" } })
+            expect( driveWrapper ).to.be.an.object();
+            driveWrapper.uploadFile('uploadTest.txt', uploadFile, { resource: { description: 'Google API uploadTest.txt test' }, properties: { testProp: "testValue" } })
                 .then(file => {
                     expect(file.id).to.exist();
                     expect(file.name).to.equal('uploadTest.txt');
@@ -65,9 +77,8 @@ lab.experiment("Google API ", {}, () => {
     lab.test("confirm uploadTest.txt", { timeout: 10000 }, () => {
 
         return new Promise((resolve, reject) => {
-            let gdriveWrapper = require('../lib/gdriveWrapper.js');
-            let wrapper = new gdriveWrapper(auth, google);
-            wrapper.getMetaForFilename('/uploadTest.txt')
+            expect( driveWrapper ).to.be.an.object();
+            driveWrapper.getMetaForFilename('/uploadTest.txt')
                 .then(file => {
                     expect(file).to.be.an.object();
                     expect(file.id).to.exist();
